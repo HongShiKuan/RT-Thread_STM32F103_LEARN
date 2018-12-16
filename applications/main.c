e44556de93a8c9dce57579a5010bab66f86743f0
WKJay <1931048074@qq.com>
2018-12-16 11:35:16 +0800

    add print thread \n delete useless files

--- applications/main.c
+++ applications/main.c
@@ -13,8 +13,47 @@
  */
 #include <rtthread.h>
 
+
+#define THREAD_PRIORITY         25
+#define THREAD_STACK_SIZE       512
+#define THREAD_TIMESLICE        5
+
+/* 指向线程控制块的指针 */
+static rt_thread_t tid1 = RT_NULL;
+
+/* 线程入口 */
+static void print_th_entry(void* parameter)
+{
+    rt_uint32_t count = 0;
+    rt_uint32_t no = (rt_uint32_t) parameter; /* 获得线程的入口参数 */
+
+    while (1)
+    {
+        /* 打印线程计数值输出 */
+        rt_kprintf("thread%d count: %d\n", no, count ++);
+
+        /* 休眠10个OS Tick */
+        rt_thread_delay(100);
+    }
+}
+
+/* 用户应用入口 */
+int mine_rt_application_init()
+{
+    /* 创建线程1 */
+    tid1 = rt_thread_create("print task",
+        print_th_entry, (void*)1, /* 线程入口是thread_entry, 入口参数是1 */
+        THREAD_STACK_SIZE, THREAD_PRIORITY, THREAD_TIMESLICE);
+    if (tid1 != RT_NULL)
+        rt_thread_startup(tid1);
+    else
+        return -1;
+    return 0;
+}
+
 int main(void)
 {
     /* user app entry */
+	mine_rt_application_init();
     return 0;
 }
